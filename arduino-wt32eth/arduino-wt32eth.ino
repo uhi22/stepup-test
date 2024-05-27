5a860da06c3dda74da80e84c2e9088a4d30f46f1
uhi22 <uhi@hnng.de>
2024-05-27 21:30:55 +0200

    added ADC. correct scaling of AD and DA values. Cleanup.

--- arduino-wt32eth/arduino-wt32eth.ino
+++ arduino-wt32eth/arduino-wt32eth.ino
@@ -3,20 +3,29 @@
 
 /* MCP4725 DAC 12 bit via I2C */
 /*   Library: "Adafruit MCP4725" */
+
+/* ADS1015 ADC 12 bit 4 channels via I2C */
+/*   Library: ADS1X15 by Rob Tillaart */
+
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
-
+#include <ADS1X15.h>
 #include "ccs32_globals.h"
 
 #define mySDA 15
 #define mySCL 14
 #define myI2CFreq 400000
 
+uint32_t currentTime;
+uint32_t lastTime50ms, lastTime500ms;
+
 TwoWire myI2C = TwoWire(1);
 Adafruit_MCP4725 dac;
+ADS1015 myADS1015(0x48, &myI2C);
 
 uint8_t testData0;
 uint16_t targetVoltage_0V1;
+uint16_t presentHighVoltage_0V1;
 
 /**********************************************************/
 /* The logging macros and functions */
@@ -51,6 +60,41 @@ void showAsHex(uint8_t *arr, uint16_t len, char *info) {
  addToTrace_chararray(strResult);
 } 
 
+/* This task runs twice per second. */
+void task500ms(void) {
+  sendTestFrame();
+  testData0++;
+}
+
+#define mV_at_400V 2470
+#define mV_at_0V 828
+#define FOURHUNDREDVOLT 4000 /* in 0.1V */
+
+void task50ms(void) {
+    int32_t tmp, u_mV;
+    tmp = targetVoltage_0V1;
+    tmp *=(mV_at_400V-mV_at_0V); /* 2500mV */
+    tmp /= FOURHUNDREDVOLT; /* at 400.0V highvoltage */
+    /* tmp is now in millivolt */
+    tmp += mV_at_0V; /* 830mV is the offset to get 0V highvoltage */
+    tmp*=4096; /* 4096 ADC full scale */
+    tmp/=3300; /* 3.3V reference voltage */
+    if (tmp>4095) tmp=4095;
+    if (tmp<0) tmp=0;
+    dac.setVoltage(tmp, false);
+
+    /* get present voltage from the ADC */
+    u_mV = myADS1015.toVoltage(myADS1015.readADC(0))*1000; /* The ADS1015 has an internal reference,
+                                                        and with the default gain it has 6.144V full scale. */
+    tmp = u_mV - mV_at_0V; /* at 0V highvoltage, the muehlpower output divided by 5k6 and 8k2 provides 828mV */
+    tmp*=FOURHUNDREDVOLT; /* 400.0V */
+    tmp/=(mV_at_400V-mV_at_0V); /* at 400V highvoltage, after the divider we have ~2500mV */
+    if (tmp<0) tmp=0; /* we do not show negative voltages */
+    presentHighVoltage_0V1 = tmp;
+}
+
+
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Started.");
@@ -66,6 +110,14 @@ void setup() {
   Serial.println("initializing DAC...");
   dac.begin(0x60, &myI2C);
   Serial.println("done");
+
+  Serial.println("initializing ADC...");
+  myADS1015.begin();
+  if (!myADS1015.isConnected()) {
+    Serial.println("Error: ADS1015 is not connected.");
+  }
+  Serial.println("done");
+
   Serial.println("Initializing ethernet...");
   if (!initEth()) {
     log_v("Error: Ethernet init failed.");
@@ -76,15 +128,19 @@ void setup() {
   //dac.setVoltage(1000, false);
   //Serial.println("done");
   //delay(500);
+  currentTime = millis();
+  lastTime50ms = currentTime;
+  lastTime500ms = currentTime;
 }
 
 void loop() { 
-    int32_t tmp;
-    tmp = targetVoltage_0V1;
-    tmp += 100;
-    if (tmp>4095) tmp=4095;
-    dac.setVoltage(tmp, false);
-    delay(10);
-    //sendTestFrame();
-    //testData0++;
+  currentTime = millis();
+  if ((currentTime - lastTime50ms)>50) {
+    lastTime50ms += 50;
+    task50ms();
+  }
+  if ((currentTime - lastTime500ms)>500) {
+    lastTime500ms += 500;
+    task500ms();
+  }
 }
--- arduino-wt32eth/homeplug.ino
+++ arduino-wt32eth/homeplug.ino
@@ -33,54 +33,12 @@
 #define MMTYPE_IND  0x0002
 #define MMTYPE_RSP  0x0003
 
-#define STATE_INITIAL  0
-#define STATE_MODEM_SEARCH_ONGOING  1
-#define STATE_READY_FOR_SLAC        2
-#define STATE_WAITING_FOR_MODEM_RESTARTED  3
-#define STATE_WAITING_FOR_SLAC_PARAM_CNF   4
-#define STATE_SLAC_PARAM_CNF_RECEIVED      5
-#define STATE_BEFORE_START_ATTEN_CHAR      6
-#define STATE_SOUNDING                     7
-#define STATE_WAIT_FOR_ATTEN_CHAR_IND      8
-#define STATE_ATTEN_CHAR_IND_RECEIVED      9
-#define STATE_DELAY_BEFORE_MATCH           10
-#define STATE_WAITING_FOR_SLAC_MATCH_CNF   11
-#define STATE_WAITING_FOR_RESTART2         12
-#define STATE_FIND_MODEMS2                 13
-#define STATE_WAITING_FOR_SW_VERSIONS      14
-#define STATE_READY_FOR_SDP                15
-#define STATE_SDP                          16
-
-#define iAmPev 1 /* This project is intended only for PEV mode at the moment. */
-#define iAmEvse 0
-
 const uint8_t MAC_BROADCAST[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 
-char strVersion[200];
-uint8_t verLen;
 uint8_t sourceMac[6];
 uint8_t localModemMac[6];
 uint8_t evseMac[6];
-uint8_t NID[7];
-uint8_t NMK[16];
-uint8_t localModemCurrentKey[16];
-uint8_t localModemFound;
-uint8_t numberOfSoftwareVersionResponses;
-uint8_t numberOfFoundModems;
-uint8_t pevSequenceState;
-uint16_t pevSequenceCyclesInState;
-uint16_t pevSequenceDelayCycles;
-uint8_t nRemainingStartAttenChar;
-uint8_t remainingNumberOfSounds;
-uint8_t AttenCharIndNumberOfSounds;
-uint8_t SdpRepetitionCounter;
-uint8_t isSDPDone;
-uint8_t sdp_state;
-uint8_t nEvseModemMissingCounter;
-
-void callbackReadyForTcp(uint8_t x) {
-  //pevStateMachine_ReInit(); /* let the PEV state machine start the TCP stuff */
-}
+uint16_t nSpecialMessageTxAliveCounter;
 
 /* Extracting the EtherType from a received message. */
 uint16_t getEtherType(uint8_t *messagebufferbytearray) {
@@ -109,83 +67,12 @@ void cleanTransmitBuffer(void) {
   }
 }
 
-void setNmkAt(uint8_t index) {
-  /* sets the Network Membership Key (NMK) at a certain position in the transmit buffer */
-  uint8_t i;
-  for (i=0; i<16; i++) {
-    mytransmitbuffer[index+i]=NMK[i]; // NMK
-  }
-}
-
-void setNidAt(uint8_t index) {
-  /* copies the network ID (NID, 7 bytes) into the wished position in the transmit buffer */
-  uint8_t i;
-  for (i=0; i<7; i++) {
-    mytransmitbuffer[index+i]=NID[i];
-  }
-}
-
 uint16_t getManagementMessageType(void) {
   /* calculates the MMTYPE (base value + lower two bits), see Table 11-2 of homeplug spec */
   return (myreceivebuffer[16]<<8) + myreceivebuffer[15];
 }
 
-void composeGetSwReq(void) {
-	/* GET_SW.REQ request, as used by the win10 laptop */
-    mytransmitbufferLen = 60;
-    cleanTransmitBuffer();
-    /* Destination MAC */
-    fillDestinationMac(MAC_BROADCAST);
-    /* Source MAC */
-    fillSourceMac(myMAC);
-    /* Protocol */
-    mytransmitbuffer[12]=0x88; // Protocol HomeplugAV
-    mytransmitbuffer[13]=0xE1; //
-    mytransmitbuffer[14]=0x00; // version
-    mytransmitbuffer[15]=0x00; // GET_SW.REQ
-    mytransmitbuffer[16]=0xA0; // 
-    mytransmitbuffer[17]=0x00; // Vendor OUI
-    mytransmitbuffer[18]=0xB0; // 
-    mytransmitbuffer[19]=0x52; //  
-}
-
-void composeSlacParamReq(void) {
-	/* SLAC_PARAM request, as it was recorded 2021-12-17 WP charger 2 */
-    mytransmitbufferLen = 60;
-    cleanTransmitBuffer();
-    // Destination MAC
-    fillDestinationMac(MAC_BROADCAST);
-    // Source MAC
-    fillSourceMac(myMAC);
-    // Protocol
-    mytransmitbuffer[12]=0x88; // Protocol HomeplugAV
-    mytransmitbuffer[13]=0xE1; //
-    mytransmitbuffer[14]=0x01; // version
-    mytransmitbuffer[15]=0x64; // SLAC_PARAM.REQ
-    mytransmitbuffer[16]=0x60; // 
-    mytransmitbuffer[17]=0x00; // 2 bytes fragmentation information. 0000 means: unfragmented.
-    mytransmitbuffer[18]=0x00; // 
-    mytransmitbuffer[19]=0x00; // 
-    mytransmitbuffer[20]=0x00; //
-    fillSourceMac(myMAC, 21); // 21 to 28: 8 bytes runid. The Ioniq uses the PEV mac plus 00 00.
-    mytransmitbuffer[27]=0x00; // 
-    mytransmitbuffer[28]=0x00; // 
-    // rest is 00
-}
-
-void evaluateSlacParamCnf(void) {
-  /* As PEV, we receive the first response from the charger. */
-  addToTrace("[PEVSLAC] Checkpoint102: received SLAC_PARAM.CNF");
-  if (iAmPev) {
-    if (pevSequenceState==STATE_WAITING_FOR_SLAC_PARAM_CNF) { //  we were waiting for the SlacParamCnf
-      pevSequenceDelayCycles = 4; // original Ioniq is waiting 200ms
-      slac_enterState(STATE_SLAC_PARAM_CNF_RECEIVED); // enter next state. Will be handled in the cyclic runSlacSequencer
-		}
-	}		
-}
-
-void composeStartAttenCharInd(void) {
-    /* reference: see wireshark interpreted frame from ioniq */
+void composeStartAttenCharIndAsSpecialMessage(void) {
     mytransmitbufferLen = 60;
     cleanTransmitBuffer();
     // Destination MAC
@@ -210,40 +97,16 @@ void composeStartAttenCharInd(void) {
     fillSourceMac(myMAC, 24); // 24 to 29: sound_forwarding_sta, MAC of the PEV
     fillSourceMac(myMAC, 30); // 30 to 37: runid, filled with MAC of PEV and two bytes 00 00
     // rest is 00 normally. We use it here for some data transmission.
-    mytransmitbuffer[38] = testData0;
-    mytransmitbuffer[39] = testData0+1;
-    mytransmitbuffer[40] = testData0+2;
-    mytransmitbuffer[41] = testData0+3;
+    mytransmitbuffer[38] = 0xDC;
+    mytransmitbuffer[39] = 0x55;
+    mytransmitbuffer[40] = 0xAA;
+    mytransmitbuffer[41] = (uint8_t)(targetVoltage_0V1>>8);
+    mytransmitbuffer[42] = (uint8_t)targetVoltage_0V1;
+    mytransmitbuffer[43] = (uint8_t)(presentHighVoltage_0V1>>8);
+    mytransmitbuffer[44] = (uint8_t)presentHighVoltage_0V1;
+    mytransmitbuffer[45] = (uint8_t)(nSpecialMessageTxAliveCounter>>8);
+    mytransmitbuffer[46] = (uint8_t)nSpecialMessageTxAliveCounter;
 
-}
-
-void composeNmbcSoundInd(void) {
-    /* reference: see wireshark interpreted frame from Ioniq */
-    uint8_t i;
-    mytransmitbufferLen = 71;
-    cleanTransmitBuffer();
-    //Destination MAC
-    fillDestinationMac(MAC_BROADCAST);
-    // Source MAC
-    fillSourceMac(myMAC);
-    // Protocol
-    mytransmitbuffer[12]=0x88; // Protocol HomeplugAV
-    mytransmitbuffer[13]=0xE1; //
-    mytransmitbuffer[14]=0x01; // version
-    mytransmitbuffer[15]=0x76; // NMBC_SOUND.IND
-    mytransmitbuffer[16]=0x60; //  
-    mytransmitbuffer[17]=0x00; // 2 bytes fragmentation information. 0000 means: unfragmented.
-    mytransmitbuffer[18]=0x00; // 
-    mytransmitbuffer[19]=0x00; // apptype
-    mytransmitbuffer[20]=0x00; // sectype
-    mytransmitbuffer[21]=0x00; // 21 to 37 sender ID, all 00
-    mytransmitbuffer[38]=remainingNumberOfSounds; // countdown. Remaining number of sounds. Starts with 9 and counts down to 0.
-    fillSourceMac(myMAC, 39); // 39 to 46: runid, filled with MAC of PEV and two bytes 00 00
-    mytransmitbuffer[47]=0x00; // 47 to 54: reserved, all 00
-    //55 to 70: random number. All 0xff in the ioniq message.
-    for (i=55; i<71; i++) { // i in range(55, 71):
-        mytransmitbuffer[i]=0xFF;
-    }
 }
 
 void evaluateAttenCharIndAsSpecialMessage(void) {
@@ -263,186 +126,9 @@ void evaluateAttenCharIndAsSpecialMessage(void) {
   }
 }
 
-void composeAttenCharRsp(void) {
-    /* reference: see wireshark interpreted frame from Ioniq */
-    mytransmitbufferLen = 70;
-    cleanTransmitBuffer();
-    // Destination MAC
-    fillDestinationMac(evseMac);
-    // Source MAC
-    fillSourceMac(myMAC);
-    // Protocol
-    mytransmitbuffer[12]=0x88; // Protocol HomeplugAV
-    mytransmitbuffer[13]=0xE1; //
-    mytransmitbuffer[14]=0x01; // version
-    mytransmitbuffer[15]=0x6F; // ATTEN_CHAR.RSP
-    mytransmitbuffer[16]=0x60; // 
-    mytransmitbuffer[17]=0x00; // 2 bytes fragmentation information. 0000 means: unfragmented.
-    mytransmitbuffer[18]=0x00; // 
-    mytransmitbuffer[19]=0x00; // apptype
-    mytransmitbuffer[20]=0x00; // sectype
-    fillSourceMac(myMAC, 21); // 21 to 26: source MAC
-    fillDestinationMac(myMAC, 27); // 27 to 34: runid. The PEV mac, plus 00 00.
-    // 35 to 51: source_id, all 00
-    // 52 to 68: resp_id, all 00
-    // 69: result. 0 is ok
-}
-		
-void composeSlacMatchReq(void) {
-    /* reference: see wireshark interpreted frame from Ioniq */
-    mytransmitbufferLen = 85;
-    cleanTransmitBuffer();
-    // Destination MAC
-    fillDestinationMac(evseMac);
-    // Source MAC
-    fillSourceMac(myMAC);
-    // Protocol
-    mytransmitbuffer[12]=0x88; // Protocol HomeplugAV
-    mytransmitbuffer[13]=0xE1; //
-    mytransmitbuffer[14]=0x01; // version
-    mytransmitbuffer[15]=0x7C; // SLAC_MATCH.REQ
-    mytransmitbuffer[16]=0x60; // 
-    mytransmitbuffer[17]=0x00; // 2 bytes fragmentation information. 0000 means: unfragmented.
-    mytransmitbuffer[18]=0x00; // 
-    mytransmitbuffer[19]=0x00; // apptype
-    mytransmitbuffer[20]=0x00; // sectype
-    mytransmitbuffer[21]=0x3E; // 21 to 22: length
-    mytransmitbuffer[22]=0x00; // 
-    // 23 to 39: pev_id, all 00
-    fillSourceMac(myMAC, 40); // 40 to 45: PEV MAC
-    // 46 to 62: evse_id, all 00
-    fillDestinationMac(evseMac, 63); // 63 to 68: EVSE MAC
-    fillSourceMac(myMAC, 69); // 69 to 76: runid. The PEV mac, plus 00 00.
-    // 77 to 84: reserved, all 00        
-}
-
-void evaluateSlacMatchCnf(void) {
-
-}
-
-void composeSetKey(void) {
-	/* CM_SET_KEY.REQ request */
-  /* From example trace from catphish https://openinverter.org/forum/viewtopic.php?p=40558&sid=9c23d8c3842e95c4cf42173996803241#p40558
-     Table 11-88 in the homeplug_av21_specification_final_public.pdf */
-  mytransmitbufferLen = 60;
-  cleanTransmitBuffer();
-  // Destination MAC
-  fillDestinationMac(MAC_BROADCAST);
-  // Source MAC
-  fillSourceMac(myMAC);
-  // Protocol
-  mytransmitbuffer[12]=0x88; // Protocol HomeplugAV
-  mytransmitbuffer[13]=0xE1; //
-  mytransmitbuffer[14]=0x01; // version
-  mytransmitbuffer[15]=0x08; // CM_SET_KEY.REQ
-  mytransmitbuffer[16]=0x60; // 
-  mytransmitbuffer[17]=0x00; // frag_index
-  mytransmitbuffer[18]=0x00; // frag_seqnum
-  mytransmitbuffer[19]=0x01; // 0 key info type
-
-  mytransmitbuffer[20]=0xaa; // 1 my nonce
-  mytransmitbuffer[21]=0xaa; // 2
-  mytransmitbuffer[22]=0xaa; // 3
-  mytransmitbuffer[23]=0xaa; // 4
-
-  mytransmitbuffer[24]=0x00; // 5 your nonce
-  mytransmitbuffer[25]=0x00; // 6
-  mytransmitbuffer[26]=0x00; // 7
-  mytransmitbuffer[27]=0x00; // 8
-        
-  mytransmitbuffer[28]=0x04; // 9 nw info pid
-        
-  mytransmitbuffer[29]=0x00; // 10 info prn
-  mytransmitbuffer[30]=0x00; // 11
-  mytransmitbuffer[31]=0x00; // 12 pmn
-  mytransmitbuffer[32]=0x00; // 13 cco cap
-  setNidAt(33); // 14-20 nid  7 bytes from 33 to 39
-                //          Network ID to be associated with the key distributed herein.
-                //          The 54 LSBs of this field contain the NID (refer to Section 3.4.3.1). The
-                //          two MSBs shall be set to 0b00.
-  mytransmitbuffer[40]=0x01; // 21 peks (payload encryption key select) Table 11-83. 01 is NMK. We had 02 here, why???
-                               // with 0x0F we could choose "no key, payload is sent in the clear"
-  setNmkAt(41); 
-  #define variation 0
-  mytransmitbuffer[41]+=variation; // to try different NMKs
-  // and three remaining zeros
-}
-
-void evaluateSetKeyCnf(void) {
- 
-}
-
-void composeGetKey(void) {
-		/* CM_GET_KEY.REQ request
-         from https://github.com/uhi22/plctool2/blob/master/listen_to_eth.c
-         and homeplug_av21_specification_final_public.pdf */
-    mytransmitbufferLen = 60;
-    cleanTransmitBuffer();
-    // Destination MAC
-    fillDestinationMac(MAC_BROADCAST);
-    // Source MAC
-    fillSourceMac(myMAC);
-    // Protocol
-    mytransmitbuffer[12]=0x88; // Protocol HomeplugAV
-    mytransmitbuffer[13]=0xE1;
-    mytransmitbuffer[14]=0x01; // version
-    mytransmitbuffer[15]=0x0C; // CM_GET_KEY.REQ https://github.com/uhi22/plctool2/blob/master/plc_homeplug.h
-    mytransmitbuffer[16]=0x60; // 
-    mytransmitbuffer[17]=0x00; // 2 bytes fragmentation information. 0000 means: unfragmented.
-    mytransmitbuffer[18]=0x00; //        
-    mytransmitbuffer[19]=0x00; // 0 Request Type 0=direct
-    mytransmitbuffer[20]=0x01; // 1 RequestedKeyType only "NMK" is permitted over the H1 interface.
-                               //           value see HomeplugAV2.1 spec table 11-89. 1 means AES-128.
-                                       
-    setNidAt(21); // NID starts here (table 11-91 Homeplug spec is wrong. Verified by accepted command.)
-    mytransmitbuffer[28]=0xaa; // 10-13 mynonce. The position at 28 is verified by the response of the devolo.
-    mytransmitbuffer[29]=0xaa; // 
-    mytransmitbuffer[30]=0xaa; // 
-    mytransmitbuffer[31]=0xaa; // 
-    mytransmitbuffer[32]=0x04; // 14 PID. According to  ISO15118-3 fix value 4, "HLE protocol"
-    mytransmitbuffer[33]=0x00; // 15-16 PRN Protocol run number
-    mytransmitbuffer[34]=0x00; // 
-    mytransmitbuffer[35]=0x00; // 17 PMN Protocol message number
-}
-
-void evaluateGetKeyCnf(void) {
-
-}
-
 void sendTestFrame(void) {
-  //composeGetSwReq();
-  composeStartAttenCharInd();
+  composeStartAttenCharIndAsSpecialMessage();
   myEthTransmit();
-}
-
-void evaluateGetSwCnf(void) {
- 
-}
-
-uint8_t isEvseModemFound(void) {
-  /* todo: look whether the MAC of the EVSE modem is in the list of detected modems */
-  /* as simple solution we say: If we see two modems, then it should be one
-     local in the car, and one in the charger. */
-  return numberOfFoundModems>1;
-}
-
-void slac_enterState(int n) {
-  addToTrace("[PEVSLAC] from " + String(pevSequenceState) + " entering " + String(n));
-  pevSequenceState = n;
-  pevSequenceCyclesInState = 0;
-}
-
-int isTooLong(void) {
-  /* The timeout handling function. */
-  return (pevSequenceCyclesInState > 500);
-}
-
-void runSlacSequencer(void) {
-
-}
-
-void runSdpStateMachine(void) {
-
 }
 
 void evaluateReceivedHomeplugPacket(void) {
@@ -451,22 +137,5 @@ void evaluateReceivedHomeplugPacket(void) {
   }
 }
 
-int homeplug_sanityCheck(void) {
-  if (pevSequenceState>STATE_SDP) {
-    addToTrace("ERROR: Sanity check of the homeplug state machine failed." + String(pevSequenceState));    
-    return -1;
-  }
-  if (sdp_state>=2) {
-    addToTrace("ERROR: Sanity check of the SDP state machine failed." + String(sdp_state));    
-    return -1;
-  }
-  return 0;  
-}
-
 void homeplugInit(void) {
-  pevSequenceState = STATE_READY_FOR_SLAC;
-  pevSequenceCyclesInState = 0;
-  pevSequenceDelayCycles = 0;
-  numberOfSoftwareVersionResponses = 0;
-  numberOfFoundModems = 0;
 }
